2b87fb5e11fb543d1dd59dacee8aa63413491acc
AnthonyBower <anthonybower6@yahoo.com>
2020-11-02 18:47:14 -0500

    Byte Int conversion methods added

--- Message.java
+++ Message.java
@@ -3,6 +3,26 @@ import java.lang.*;
 
 public class Message {
 
+    private static int bytesToInt(byte[] bytes) {
+        if(byets.length != 4) {
+            //ERROR
+        }
+
+        return  (bytes[0] & 0xff) << 24 +
+                (bytes[1] & 0xff) << 16 +
+                (bytes[2] & 0xff) << 8  +
+                (bytes[3] & 0xff) << 0;
+    }
+
+    private static byte[] intToBytes(int val) {
+        byte[] bytes = {    (byte) ( (id >> 24) & 0xff ),
+                            (byte) ( (id >> 16) & 0xff ),
+                            (byte) ( (id >> 8)  & 0xff ),
+                            (byte) ( (id >> 0)  & 0xff ) };
+
+        return bytes;
+    }
+
     public static final byte CHOKE = 0;
     public static final byte UNCHOKE = 1;
     public static final byte INTERESTED = 2;
@@ -36,10 +56,7 @@ public class Message {
 
         Arrays.fill(msg, header.length, header.length + 10, 0);
 
-        byte[] idb = {  (byte) ( (id >> 24) & 0xff ),
-                        (byte) ( (id >> 16) & 0xff ),
-                        (byte) ( (id >> 8)  & 0xff ),
-                        (byte) ( (id >> 0)  & 0xff ) };
+        byte[] idb = intToBytes(id)
 
         Systems.arraycopy(idb, 0, msg, header.length + 10, idb.length);
 
